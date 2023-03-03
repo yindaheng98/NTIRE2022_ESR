ebb34e3d3823f47a032352d30616a70f3a93824b
yindaheng98 <yindaheng98@gmail.com>
2023-03-03 10:41:33 +0800

    clean result

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ eval "$(conda shell.bash hook)"
 conda activate NTIRE23
 ROOT=$(dirname $0)
 
+rm -rf "$ROOT/results"
+rm results.json
 mkdir -p "$ROOT/results"
 CUDA_VISIBLE_DEVICES=0 python test_demo.py \
   --lr_dir /home/data/dataset/DIV2K/DIV2K_valid_LR_bicubic/X4 \
   --hr_dir /home/data/dataset/DIV2K/DIV2K_valid_HR \
   --save_dir "$ROOT/results" \
-  --model_id -1
+  --model_id 0
 cat results.json | jq
