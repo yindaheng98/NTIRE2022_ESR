89d3255086f42c3f65478ef34b6def41f5b7d2ac
yindaheng98 <yindaheng98@gmail.com>
2023-03-06 21:15:09 +0800

    参数量为什么不降？

---
+++ run_prune.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=/home/seu/miniconda3/envs/NTIRE23/lib/python3.9/site-packages/torch/lib/../../nvidia/cublas/lib/:$LD_LIBRARY_PATH
+eval "$(conda shell.bash hook)"
+conda activate NTIRE23
+ROOT=$(dirname $0)
+
+rm -rf "$ROOT/results"
+mkdir -p "$ROOT/results"
+CUDA_VISIBLE_DEVICES=1 python test_prune.py \
+  --lr_dir /home/data/dataset/DIV2K/DIV2K_valid_LR_bicubic/X4 \
+  --hr_dir /home/data/dataset/DIV2K/DIV2K_valid_HR \
+  --save_dir "$ROOT/results" \
+  --model_id 0
---
+++ test_prune.py
@@ -0,0 +1,93 @@
+import argparse
+import os
+from pprint import pprint
+
+import torch
+import torch_pruning as tp
+
+from test_demo import select_model, select_dataset, util, get_model_activation, get_model_flops
+
+
+@torch.no_grad()
+def count_ops_and_params(model):
+    input_dim = (3, 256, 256)  # set the input dimension
+    activations, num_conv = get_model_activation(model, input_dim)
+    flops = get_model_flops(model, input_dim, False)
+    return activations, num_conv, flops
+
+
+prune_ignores = {
+    0: lambda m: [m.upsampler[0]]
+}
+
+
+def main(args):
+    # --------------------------------
+    # load model
+    # --------------------------------
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model, model_name, data_range, tile = select_model(args, device)
+
+    # --------------------------------
+    # dataset path
+    # --------------------------------
+    mode = "valid"
+    data_path = select_dataset(args.lr_dir, args.hr_dir, mode)
+    save_path = os.path.join(args.save_dir, model_name, mode)
+    util.mkdir(save_path)
+
+    # --------------------------------
+    # load example inputs
+    # --------------------------------
+    input_dim = (3, 256, 256)
+    example_inputs = torch.FloatTensor(1, *input_dim).to(device)
+
+    # --------------------------------
+    # load pruner
+    # --------------------------------
+    imp = tp.importance.MagnitudeImportance(p=2)
+    pruner = tp.pruner.MagnitudePruner(
+        model,
+        example_inputs,
+        importance=imp,
+        iterative_steps=len(data_path),  # progressive pruning
+        ch_sparsity=0.5,  # remove 50% channels, ResNet18 = {64, 128, 256, 512} => ResNet18_Half = {32, 64, 128, 256}
+        ignored_layers=prune_ignores[args.model_id](model) if args.model_id in prune_ignores else [],
+    )
+
+    # --------------------------------
+    # pruning
+    # --------------------------------
+    base_macs, base_nparams = tp.utils.count_ops_and_params(model, example_inputs)
+    for i, (img_lr, img_hr) in enumerate(data_path):
+        pruner.step()
+        macs, nparams = tp.utils.count_ops_and_params(model, example_inputs)
+        print(model)
+        print(model(example_inputs).shape)
+        print(
+            "  Iter %d/%d, Params: %.2f M => %.2f M"
+            % (i + 1, len(data_path), base_nparams / 1e6, nparams / 1e6)
+        )
+        print(
+            "  Iter %d/%d, MACs: %.2f G => %.2f G"
+            % (i + 1, len(data_path), base_macs / 1e9, macs / 1e9)
+        )
+        # finetune your model here
+        # finetune(model)
+        # ...
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("NTIRE2022-EfficientSR")
+    parser.add_argument("--lr_dir", default="/cluster/work/cvl/yawli/data/NTIRE2022_Challenge", type=str)
+    parser.add_argument("--hr_dir", default="/cluster/work/cvl/yawli/data/NTIRE2022_Challenge", type=str)
+    parser.add_argument("--save_dir", default="/cluster/work/cvl/yawli/data/NTIRE2022_Challenge/results", type=str)
+    parser.add_argument("--model_id", default=0, type=int)
+    parser.add_argument("--include_test", action="store_true", help="Inference on the DIV2K test set")
+    parser.add_argument("--ssim", action="store_true", help="Calculate SSIM")
+    parser.add_argument("--onnx", default=None, type=str, help="Save to onnx")
+
+    args = parser.parse_args()
+    pprint(args)
+
+    main(args)
