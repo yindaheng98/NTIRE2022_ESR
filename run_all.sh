7adffd7a38885f8efa9c0a8726d936db22c0bbf7
yindaheng98 <yindaheng98@gmail.com>
2023-03-06 20:27:51 +0800

    写文件会冲突，还是一个个测吧

--- run_all.sh
+++ run_all.sh
@@ -4,19 +4,14 @@ eval "$(conda shell.bash hook)"
 conda activate NTIRE23
 ROOT=$(dirname $0)
 
-for i in {0..4}; do
-  for j in {0..7}; do
-    rm -rf "$ROOT/results"
-    mkdir -p "$ROOT/results"
-    echo $(($i * 8 + $j))
-    CUDA_VISIBLE_DEVICES=$j python test_demo.py \
-      --lr_dir /home/data/dataset/DIV2K/DIV2K_valid_LR_bicubic/X4 \
-      --hr_dir /home/data/dataset/DIV2K/DIV2K_valid_HR \
-      --save_dir "$ROOT/results" \
-      --model_id $(($i * 8 + $j)) &
-  done
-  echo "waiting"
-  wait
+for i in {0..44}; do
+rm -rf "$ROOT/results"
+mkdir -p "$ROOT/results"
+CUDA_VISIBLE_DEVICES=1 python test_demo.py \
+  --lr_dir /home/data/dataset/DIV2K/DIV2K_valid_LR_bicubic/X4 \
+  --hr_dir /home/data/dataset/DIV2K/DIV2K_valid_HR \
+  --save_dir "$ROOT/results" \
+  --model_id -2
 done
 printf "%20s %12s %17s %14s %5s\n" model_name valid_memory valid_ave_runtime valid_ave_psnr flops
 for line in $(cat results.json | jq -r 'to_entries|.[]|[.key,.value.valid_memory,.value.valid_ave_runtime,.value.valid_ave_psnr,.value.flops|tostring] | join(",")'); do
