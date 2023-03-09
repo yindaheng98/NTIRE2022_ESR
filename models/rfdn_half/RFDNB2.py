5b60fc5c045ce9d09df4ac1a911232315239bded
yindaheng98 <yindaheng98@gmail.com>
2023-03-09 22:36:36 +0800

    merge branch into one

--- models/rfdn_half/RFDNB2.py
+++ models/rfdn_half/RFDNB2.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 import models.rfdn_baseline.block as B
 from .RFDN import RFDN
@@ -18,6 +19,40 @@ class RFDNB2(RFDN):
 
         out_B = self.c(torch.cat([out_B1, out_B2, out_B3, out_B4], dim=1))
         out_lr = self.LR_conv(out_B) + out_fea
+
+        output = self.upsampler(out_lr)
+
+        return output
+
+
+class RFDNB2_P(nn.Module):
+    def __init__(self, model: RFDNB2, in_nc=3, nf=50, num_modules=4, out_nc=3, upscale=4):
+        super().__init__()
+
+        self.fea_conv12 = B.conv_layer(in_nc, nf * 2, kernel_size=3)
+
+        self.B13 = B.RFDB(in_channels=nf * 2)
+        self.B24 = B.RFDB(in_channels=nf * 2)
+        self.c = B.conv_block(nf * num_modules, nf, kernel_size=1, act_type='lrelu')
+
+        self.LR_conv = B.conv_layer(nf, nf, kernel_size=3)
+
+        upsample_block = B.pixelshuffle_block
+        self.upsampler = upsample_block(nf, out_nc, upscale_factor=4)
+        self.scale_idx = 0
+
+    def forward(self, input):
+        out_fea12 = self.fea_conv12(input)
+        out_B13 = self.B13(out_fea12)
+        out_B24 = self.B24(out_B13)
+
+        out_B = self.c(torch.cat([
+            out_B13[:, 0:out_B13.shape[1] // 2],
+            out_B24[:, 0:out_B24.shape[1] // 2],
+            out_B13[:, out_B13.shape[1] // 2:],
+            out_B24[:, out_B24.shape[1] // 2:],
+        ], dim=1))
+        out_lr = self.LR_conv(out_B) + out_fea12[:, 0:out_fea12.shape[1] // 2]
 
         output = self.upsampler(out_lr)
 
--- models/rfdn_half/RFDNB4.py
+++ models/rfdn_half/RFDNB4.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 import models.rfdn_baseline.block as B
 from .RFDNB2 import RFDNB2
@@ -19,6 +20,33 @@ class RFDNB4(RFDNB2):
 
         out_B = self.c(torch.cat([out_B1, out_B2, out_B3, out_B4], dim=1))
         out_lr = self.LR_conv(out_B) + out_fea
+
+        output = self.upsampler(out_lr)
+
+        return output
+
+
+class RFDNB4_P(nn.Module):
+    def __init__(self, model: RFDNB4, in_nc=3, nf=50, num_modules=4, out_nc=3, upscale=4):
+        super().__init__()
+
+        self.fea_conv1234 = B.conv_layer(in_nc, nf * 4, kernel_size=3)
+
+        self.B1234 = B.RFDB(in_channels=nf * 4)
+        self.c = B.conv_block(nf * num_modules, nf, kernel_size=1, act_type='lrelu')
+
+        self.LR_conv = B.conv_layer(nf, nf, kernel_size=3)
+
+        upsample_block = B.pixelshuffle_block
+        self.upsampler = upsample_block(nf, out_nc, upscale_factor=4)
+        self.scale_idx = 0
+
+    def forward(self, input):
+        out_fea1234 = self.fea_conv1234(input)
+        out_B1234 = self.B1234(out_fea1234)
+
+        out_B = self.c(out_B1234)
+        out_lr = self.LR_conv(out_B) + out_fea1234[:, 0:out_fea1234.shape[1] // 4]
 
         output = self.upsampler(out_lr)
 
