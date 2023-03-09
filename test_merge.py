a945d0e948de167dd5421761f486551536b75cfc
yindaheng98 <yindaheng98@gmail.com>
2023-03-09 23:18:07 +0800

    ESA 没问题

--- test_merge.py
+++ test_merge.py
@@ -3,22 +3,17 @@ import random
 import torch
 import torch.nn as nn
 
-from models.rfdn_half.block import conv_layer
-from models.rfdn_half.block import conv_p
-from models.rfdn_half.block import conv_layer_p
+from models.rfdn_baseline.block import ESA
+from models.rfdn_half.block import ESA_P, conv_p
 
 
 def main():
     in_channels = random.randint(4, 10)
-    out_channels = random.randint(4, 10)
-    kernel_size = random.randint(4, 10)
     convs = [
-        conv_layer(in_channels=in_channels,
-                   out_channels=out_channels,
-                   kernel_size=kernel_size)
+        ESA(n_feats=in_channels, conv=nn.Conv2d)
         for _ in range(random.randint(4, 10))
     ]
-    conv = conv_layer_p(convs, in_channels, out_channels, kernel_size)
+    conv = ESA_P(convs, n_feats=in_channels, conv=conv_p)
     batch = random.randint(4, 10)
     inputs = [torch.randn(batch, in_channels, 224, 224) for i in range(len(convs))]
     conv_output = torch.cat([c(i) for c, i in zip(convs, inputs)], dim=1)
