4eece14a7c76da83be825cfc8b7f7115737db6ba
yindaheng98 <yindaheng98@gmail.com>
2023-03-10 10:44:11 +0800

    tail 化 B4

--- models/rfdn_half/RFDNB4.py
+++ models/rfdn_half/RFDNB4.py
@@ -13,11 +13,13 @@ class RFDNB4(RFDNB2):
         self.fea_conv3 = B.conv_layer(in_nc, nf, kernel_size=3)
 
     def forward(self, input):
-        out_fea = self.fea_conv(input)
+        return self.tail(self.fea_conv(input), self.fea_conv1(input), self.fea_conv2(input), self.fea_conv3(input))
+
+    def tail(self, out_fea, out_fea1, out_fea2, out_fea3):
         out_B1 = self.B1(out_fea)
-        out_B2 = self.B2(self.fea_conv1(input))
-        out_B3 = self.B3(self.fea_conv2(input))
-        out_B4 = self.B4(self.fea_conv3(input))
+        out_B2 = self.B2(out_fea1)
+        out_B3 = self.B3(out_fea2)
+        out_B4 = self.B4(out_fea3)
 
         out_B = self.c(torch.cat([out_B1, out_B2, out_B3, out_B4], dim=1))
         out_lr = self.LR_conv(out_B) + out_fea
@@ -41,7 +43,9 @@ class RFDNB4_P(nn.Module):
         self.scale_idx = 0
 
     def forward(self, input):
-        out_fea1234 = self.fea_conv1234(torch.cat([input, input, input, input], dim=1))
+        return self.tail(self.fea_conv1234(torch.cat([input, input, input, input], dim=1)))
+
+    def tail(self, out_fea1234):
         out_B1234 = self.B1234(out_fea1234)
 
         out_B = self.c(out_B1234)
