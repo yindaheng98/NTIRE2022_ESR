214e574de7b6777ef278c5371cf64890d244cef1
yindaheng98 <yindaheng98@gmail.com>
2023-03-10 10:34:19 +0800

    B2S ok

--- models/rfdn_half/RFDNB2S.py
+++ models/rfdn_half/RFDNB2S.py
@@ -1,7 +1,7 @@
 import torch
 
-from .RFDNB2 import RFDNB2
-from .block import RFDBS
+from .RFDNB2 import RFDNB2, RFDNB2_P
+from .block import RFDBS, conv_layer_p
 
 
 class RFDNB2S(RFDNB2):
@@ -18,6 +18,30 @@ class RFDNB2S(RFDNB2):
 
         out_B = self.c(torch.cat([out_B1, out_B2, out_B3, out_B4], dim=1))
         out_lr = self.LR_conv(out_B) + out_fea
+
+        output = self.upsampler(out_lr)
+
+        return output
+
+
+class RFDNB2S_P(RFDNB2_P):
+    def __init__(self, model: RFDNB2S, in_nc=3, nf=50):
+        super().__init__(model, in_nc=in_nc, nf=nf)
+        self.B2S2 = model.B2S2
+
+    def forward(self, input):
+        out_fea12 = self.fea_conv12(torch.cat([input, input], dim=1))
+        out_fea12[:, out_fea12.shape[1] // 2:, ...] = self.B2S2(out_fea12[:, out_fea12.shape[1] // 2:, ...])
+        out_B13 = self.B13(out_fea12)
+        out_B24 = self.B24(out_B13)
+
+        out_B = self.c(torch.cat([
+            out_B13[:, 0:out_B13.shape[1] // 2],
+            out_B24[:, 0:out_B24.shape[1] // 2],
+            out_B13[:, out_B13.shape[1] // 2:],
+            out_B24[:, out_B24.shape[1] // 2:],
+        ], dim=1))
+        out_lr = self.LR_conv(out_B) + out_fea12[:, 0:out_fea12.shape[1] // 2]
 
         output = self.upsampler(out_lr)
 
