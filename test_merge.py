07bab1bc5ef07711fa8dd40b2972865c3a2b17cd
yindaheng98 <yindaheng98@gmail.com>
2023-03-10 10:56:34 +0800

    值搞大点

--- test_merge.py
+++ test_merge.py
@@ -5,6 +5,7 @@ import torch
 # from models.rfdn_half.RFDNB2 import RFDNB2 as model, RFDNB2_P as model_p
 from models.rfdn_half.RFDNB2S import RFDNB2S as model, RFDNB2S_P as model_p
 # from models.rfdn_half.RFDNB4 import RFDNB4 as model, RFDNB4_P as model_p
+# from models.rfdn_half.RFDNB4S import RFDNB4S as model, RFDNB4S_P as model_p
 
 
 def main():
@@ -12,13 +13,13 @@ def main():
     convs = model(in_nc=in_channels)
     conv = model_p(convs, in_nc=in_channels)
     batch = random.randint(4, 10)
-    inp = torch.randn(batch, in_channels, 224, 224)
+    inp = torch.randn(batch, in_channels, 224, 224) + 1000
     conv_output = conv(inp)
     print(conv_output.shape)
     convs_output = convs(inp)
     print(convs_output.shape)
     print(conv_output - convs_output)
-    print(torch.sum(torch.abs(conv_output - convs_output)))
+    print(torch.max(torch.abs(conv_output - convs_output)))
 
 
 if __name__ == "__main__":
